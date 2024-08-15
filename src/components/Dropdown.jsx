ee051baff76cb5de8f59114f4b62d91416be6410
vishaldhakal <vishaldhakal96@gmail.com>
2024-08-15 08:59:14 +0545

    chore: Update Dropdown component styles and options grid layout

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -41,7 +41,7 @@ const Dropdown = ({ name, options, width = "25rem" }) => {
       </button>
 
       <div
-        className={`absolute left-[-1rem] sm:right-0 mt-6 bg-white rounded-md shadow-shuttle z-10 text-sm h-auto grid ${
+        className={`absolute left-[-1rem] sm:right-0 mt-6 bg-white rounded-md shadow-2xl border z-10 text-sm h-auto grid ${
           options.length > 5
             ? "grid-cols-3 sm:w-[30rem] w-[20rem] "
             : "grid-cols-1 w-[16rem]"
