7b17e23cf93e7c95a421a44f84760fb5074cc173
vishaldhakal <vishaldhakal96@gmail.com>
2024-08-15 09:36:07 +0545

    chore: Update image imports to use <img> instead of next/image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -26,14 +26,14 @@ const Footer = ({ cities }) => {
               <h3 className="text-5xl font-extrabold text-center mb-12 font-family2">
                 Explore New Homes For Sale In Canada
               </h3>
-              <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 text-sm">
+              <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-4 text-sm">
                 {allCities.map((val, idx) => (
                   <li key={idx} className="text-center">
                     <Link
                       href={generateURL({ cityVal: val.city })}
                       className="hover:text-primary-green transition-colors"
                     >
-                      New homes in {val.city}
+                      Homes for sale in {val.city}
                     </Link>
                   </li>
                 ))}
@@ -56,16 +56,18 @@ const Footer = ({ cities }) => {
                   </Link>
                 </div>
                 <p className="text-sm leading-relaxed mb-8">
-                  Lowrise is Canada's one of the largest databases of new
-                  pre-construction homes. Our comprehensive database is
-                  populated by our research and analysis of publicly available
-                  data. Lowrise strives for accuracy and we make every effort to
-                  verify the information. The information provided on Lowrise.ca
-                  may be outdated or inaccurate. Lowrise is not liable for the
-                  use or misuse of the site's information. The information
-                  displayed on Lowrise.ca is for reference only. Please contact
-                  a licensed real estate agent or broker to seek advice or
-                  receive updated and accurate information.
+                  Lowrise refers potential buyers to real estate agents that are
+                  licensed in the province where the respective property is
+                  located. Dolphin Realty is licensed as a real estate brokerage
+                  in Ontario.
+                  <br />
+                  IDX information is provided exclusively for consumers’
+                  personal, non-commercial use and that it may not be used for
+                  any purpose other than to identify prospective properties
+                  consumers may be interested in purchasing. The information
+                  displayed herein is not intended to solicit a trade in real
+                  estate. Information deemed reliable but not guaranteed to be
+                  accurate. Listing information updated daily.
                 </p>
                 <div className="space-y-6">
                   <div>
@@ -114,20 +116,57 @@ const Footer = ({ cities }) => {
 
           {/* Logo and copyright */}
           <div className="mt-16 pt-8 border-t border-gray-200">
-            <div className="flex flex-col items-center space-y-4">
-              <p className="text-xs text-gray-500 max-w-2xl text-center">
-                This representation is based in whole or part on data generated
-                by the Toronto Regional Real Estate Board. The Toronto Regional
-                Real Estate Board and the page owner assumes no responsibility
-                for its accuracy. The materials contained on this page may not
-                be reproduced without the express written consent of the Toronto
-                Regional Real Estate Board and the page owner.
-              </p>
-              <img src="/logo/trebb.png" alt="TREBB Logo" className="w-28" />
-              <p className="text-sm">
-                ©2024 Copyright Lowrise All Rights Reserved
-              </p>
+            <p className="text-xs leading-relaxed mb-8 text-center">
+              The trademarks REALTOR®, REALTORS®, and the REALTOR® logo are
+              controlled by The Canadian Real Estate Association (CREA) and
+              identify real estate professionals who are members of CREA. The
+              trademarks, Multiple Listing Service® and the associated logos are
+              owned by CREA and identify the quality of services provided by
+              real estate professionals who are members of CREA. Used under
+              license.
+            </p>
+            <div className="grid grid-cols-1 md:grid-cols-2 items-center space-y-4 gap-4">
+              <div className="flex flex-col items-center space-y-4">
+                <img src="/logo/trebb.png" alt="TREBB Logo" className="w-28" />
+                <p className="text-xs text-gray-500 max-w-2xl text-center">
+                  Toronto Real Estate Board (TRREB); All information deemed
+                  reliable but not guaranteed. All properties are subject to
+                  prior sale, change or withdrawal. Neither listing broker(s) or
+                  information provider(s) shall be responsible for any
+                  typographical errors, misinformation, misprints and shall be
+                  held totally harmless. Listing(s) information is provided for
+                  consumer's personal, non-commercial use and may not be used
+                  for any purpose other than to identify prospective properties
+                  consumers may be interested in purchasing. The data relating
+                  to real estate for sale on this website comes in part from the
+                  Internet Data Exchange program of the Multiple Listing
+                  Service. Real estate listings held by brokerage firms other
+                  than Dolphin Realty Inc. may be marked with the Internet Data
+                  Exchange logo and detailed information about those properties
+                  will include the name of the listing broker(s) when required
+                  by the MLS. Copyright ©2024 All rights reserved. Last Updated:
+                  August 15, 2024 2:32 AM UTC
+                </p>
+              </div>
+              <div className="flex flex-col items-center space-y-4">
+                <img src="/crea.png" alt="TREBB Logo" className="w-28" />
+                <p className="text-xs text-gray-500 max-w-2xl text-center">
+                  The listing data displayed is deemed reliable but is not
+                  guaranteed accurate by CREA®. Data last updated on August 14,
+                  2024 6:32 AM UTC The trademarks REALTOR®, REALTORS®; and the
+                  REALTOR® logo are controlled by The Canadian Real Estate
+                  Association (CREA®) and identify real estate professionals who
+                  are members of CREA®. Used under license. The trademarks MLS®,
+                  Multiple Listing Service® and the associated logos are owned
+                  by The Canadian Real Estate Association (CREA®) and identify
+                  the quality of services provided by real estate professionals
+                  who are members of CREA®. Used under license.
+                </p>
+              </div>
             </div>
+            <p className="text-sm text-center mt-10">
+              ©2024 Copyright Lowrise All Rights Reserved
+            </p>
           </div>
         </div>
       </footer>
