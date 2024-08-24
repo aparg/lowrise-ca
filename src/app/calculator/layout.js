47267c79911eb3d46fdf625f2456860773183488
aparg <apargtm@gmail.com>
2024-08-24 13:02:24 +0545

    fix: blogs spacing

--- src/app/calculator/layout.js
+++ src/app/calculator/layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const layout = ({ children }) => {
-  return <div className="max-w-[90%]">{children}</div>;
+  return <div className="max-w-[90%] mx-auto">{children}</div>;
 };
 
 export default layout;
