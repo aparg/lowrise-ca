b6fe2e203beca84ce068a597ad53ffd0facb3591
aparg <apargtm@gmail.com>
2024-09-01 15:48:03 +0545

    style: disclaimer page

--- src/app/disclaimer/page.js
+++ src/app/disclaimer/page.js
@@ -1,49 +1,88 @@
 import React from "react";
+import { FaExclamationTriangle } from "react-icons/fa";
 
 const page = () => {
   return (
-    <div className="max-w-[90%] mx-auto">
-      <h1 className="font-extrabold text-4xl my-4">
-        Disclaimer: Understanding Average Price Calculations for Resale Homes
-      </h1>
-      <ol className="list-decimal">
-        <li className="my-3">
-          How We Calculate the Average Price The average prices displayed on
-          Lowrise.ca are calculated using the sales data of properties within
-          specific categories (Townhomes, Detached homes, Triplexes, and
-          Duplexes) over a defined period. We take the total sum of the sale
-          prices of all properties in a given category and divide it by the
-          number of properties to arrive at an average price. This average gives
-          a general indication of the market trend within each property
-          category.{" "}
-        </li>
-        <li className="my-3">
-          Number of Properties Considered To provide a reliable average, we
-          include a substantial number of property sales in our calculations.
-          The number of properties considered may vary depending on the
-          availability of data for each category during the selected timeline.
-          The larger the sample size, the more representative the average price
-          is of the current market conditions.
-        </li>
-        <li className="my-3">
-          Disclaimer: Limitations of Average Price Data While we strive to
-          provide accurate data, it is important to note that average prices can
-          be influenced by various factors. Certain properties, such as those
-          with unique features, exceptionally high or low sale prices, or
-          located in particularly desirable or less desirable areas, may skew
-          the average price data. Therefore, the average prices provided should
-          be considered as general market indicators rather than precise
-          reflections of every property’s value.
-        </li>
-        <li className="my-3">
-          Timeline of Data The average prices presented on Lowrise.ca are based
-          on sales data from the past [insert timeframe, e.g., "6 months," "12
-          months"]. This timeline allows us to account for recent market changes
-          while providing a broad overview of price trends. Please note that
-          market conditions can change rapidly, and past averages may not
-          accurately predict future prices.
-        </li>
-      </ol>
+    <div className="min-h-screen py-8 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-3xl mx-auto bg-white shadow-[0_0_50px_#EE426680] shadow-xl rounded-lg overflow-hidden">
+        <div className="bg-primary-green py-4 px-6">
+          <h1 className="text-3xl font-bold text-white flex items-center">
+            <FaExclamationTriangle className="mr-2" />
+            Disclaimer
+          </h1>
+        </div>
+        <div className="p-6">
+          <h2 className="text-2xl font-semibold text-gray-800 mb-6">
+            Understanding Average Price Calculations for Resale Homes
+          </h2>
+          <ol className="list-decimal list-outside ml-6 space-y-6 marker:text-red-500 marker:font-bold">
+            <li>
+              <h3 className="font-bold text-lg text-red-500 mb-2">
+                How We Calculate the Average Price
+              </h3>
+              <p className="text-gray-600">
+                The average prices on Lowrise.ca are calculated using sales data
+                of properties within specific categories (Townhomes, Detached
+                homes, Triplexes, and Duplexes) over a defined period. We take
+                the total sum of sale prices and divide it by the number of
+                properties to arrive at an average price, giving a general
+                indication of market trends within each category.
+              </p>
+            </li>
+            <li>
+              <h3 className="font-bold text-lg text-red-500 mb-2">
+                Number of Properties Considered
+              </h3>
+              <p className="text-gray-600">
+                To provide reliable averages, we include a substantial number of
+                property sales in our calculations. The sample size may vary
+                depending on data availability for each category during the
+                selected timeline. Larger sample sizes provide more
+                representative average prices of current market conditions.
+              </p>
+            </li>
+            <li>
+              <h3 className="font-bold text-lg text-red-500 mb-2">
+                Limitations of Average Price Data
+              </h3>
+              <p className="text-gray-600">
+                Average prices can be influenced by various factors. Properties
+                with unique features, exceptionally high or low sale prices, or
+                those in particularly desirable or less desirable areas may skew
+                the data. Consider these averages as general market indicators
+                rather than precise reflections of every property's value.
+              </p>
+            </li>
+            <li>
+              <h3 className="font-bold text-lg text-red-500 mb-2">
+                Timeline of Data
+              </h3>
+              <p className="text-gray-600">
+                Average prices are based on sales data from the past [insert
+                timeframe, e.g., "6 months," "12 months"]. This timeline
+                accounts for recent market changes while providing a broad
+                overview of price trends. Note that market conditions can change
+                rapidly, and past averages may not accurately predict future
+                prices.
+              </p>
+            </li>
+            <li>
+              <h3 className="font-bold text-lg text-red-500 mb-2">
+                Accuracy of Calculations and User Responsibility
+              </h3>
+              <p className="text-gray-600">
+                While we strive for accuracy in our calculations, data may
+                sometimes be incomplete, outdated, or inaccurate. We strongly
+                recommend users conduct their own research and consult with real
+                estate professionals before making decisions based on
+                information from Lowrise.ca. Our data should be used as a
+                general guide rather than the sole basis for financial or real
+                estate transactions.
+              </p>
+            </li>
+          </ol>
+        </div>
+      </div>
     </div>
   );
 };
