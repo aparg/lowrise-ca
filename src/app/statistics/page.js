5e91a1445e456fa123442245f5e92d8cc807eb42
aparg <apargtm@gmail.com>
2024-09-22 17:22:46 +0545

    fix: carousel smaller when scrolled

--- src/app/statistics/page.js
+++ src/app/statistics/page.js
@@ -44,7 +44,7 @@ const page = async () => {
 
   return (
     <div className="container mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-4 text-center">
+      <h1 className="text-3xl font-bold mb-4 text-center">
         Statistics for Townhomes in Brampton, ON
       </h1>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
@@ -71,56 +71,59 @@ const page = async () => {
         </div>
 
         {/* Calculation Section */}
-        <div className="bg-gray-50 shadow-md rounded-lg p-6">
-          <h2 className="text-xl font-semibold mb-4">Calculations</h2>
-          <p className="text-lg mb-2">Total Properties: {totalProperties}</p>
-          <p className="text-lg mb-2">
-            Total Price: ${totalPrice.toLocaleString()}
-          </p>
-          {/* Mean Section */}
-          <div className="mb-6 bg-slate-200 p-5 rounded-md">
-            <h3 className="text-lg font-semibold">Mean Price</h3>
-
-            <p className="text-lg mb-4">Mean Price: ${meanPrice}</p>
-            <div className="border-t pt-2 mt-2">
-              <h4 className="text-md font-semibold">Formula</h4>
-              <p className="text-gray-700">
-                <code>Mean Price = (Total Price) ÷ (Total Properties)</code>
-              </p>
-            </div>
-          </div>
-
-          {/* Median Section */}
-          <div className="mb-6 bg-slate-200 p-5 rounded-md">
-            <h3 className="text-lg font-semibold">Median Price</h3>
-            <p className="text-lg mb-4">Median Price: ${medianPrice}</p>
-            <div className="border-t pt-2 mt-2">
-              <h4 className="text-md font-semibold">Formula</h4>
-              <p className="text-gray-700">
-                <code>Median = Middle value when prices are sorted</code>
-              </p>
-              <p className="text-gray-700 mt-1 italic">
-                If the number of properties is odd, the median is the middle
-                value. If even, it's the average of the two middle values.
-              </p>
-            </div>
-          </div>
-
-          {/* Standard Deviation Section */}
-          <div className="mb-6 bg-slate-200 p-5 rounded-md">
-            <h3 className="text-lg font-semibold">Standard Deviation</h3>
-            <p className="text-lg mb-4">
-              Standard Deviation: ${standardDeviation}
+        <div>
+          <div className="bg-gray-50 shadow-md rounded-lg p-6">
+            <h2 className="text-xl font-semibold mb-4">Calculations</h2>
+            <p className="text-lg mb-2">Total Properties: {totalProperties}</p>
+            <p className="text-lg mb-2">
+              Total Price: ${totalPrice.toLocaleString()}
             </p>
-            <div className="border-t pt-2 mt-2">
-              <h4 className="text-md font-semibold">Formula</h4>
-              <p className="text-gray-700">
-                <code>σ = √(Σ(price - mean)² / N)</code>
+            {/* Mean Section */}
+            <div className="mb-6 bg-slate-200 p-5 rounded-md">
+              <h3 className="text-lg font-semibold">Mean Price</h3>
+
+              <p className="text-lg mb-4">Mean Price: ${meanPrice}</p>
+              <div className="border-t pt-2 mt-2">
+                <h4 className="text-md font-semibold">Formula</h4>
+                <p className="text-gray-700">
+                  <code>Mean Price = (Total Price) ÷ (Total Properties)</code>
+                </p>
+              </div>
+            </div>
+
+            {/* Median Section */}
+            <div className="mb-6 bg-slate-200 p-5 rounded-md">
+              <h3 className="text-lg font-semibold">Median Price</h3>
+              <p className="text-lg mb-4">Median Price: ${medianPrice}</p>
+              <div className="border-t pt-2 mt-2">
+                <h4 className="text-md font-semibold">Formula</h4>
+                <p className="text-gray-700">
+                  <code>Median = Middle value when prices are sorted</code>
+                </p>
+                <p className="text-gray-700 mt-1 italic">
+                  If the number of properties is odd, the median is the middle
+                  value. If even, it's the average of the two middle values.
+                </p>
+              </div>
+            </div>
+
+            {/* Standard Deviation Section */}
+            <div className="mb-6 bg-slate-200 p-5 rounded-md">
+              <h3 className="text-lg font-semibold">Standard Deviation</h3>
+              <p className="text-lg mb-4">
+                Standard Deviation: ${standardDeviation}
               </p>
-              <p className="text-gray-700 mt-1 italic">
-                Calculate the squared differences from the mean, take their
-                average, and then the square root to get the standard deviation.
-              </p>
+              <div className="border-t pt-2 mt-2">
+                <h4 className="text-md font-semibold">Formula</h4>
+                <p className="text-gray-700">
+                  <code>σ = √(Σ(price - mean)² / N)</code>
+                </p>
+                <p className="text-gray-700 mt-1 italic">
+                  Calculate the squared differences from the mean, take their
+                  average, and then the square root to get the standard
+                  deviation.
+                </p>
+              </div>
             </div>
           </div>
         </div>
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,14 +1,28 @@
 "use client";
 import React, { useState, useEffect, useRef } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import Link from "next/link";
 
 const Carousel = ({ urls }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isSticky, setIsSticky] = useState(false);
   const scrollRef = useRef();
+  const stickyRef = useRef();
 
   useEffect(() => {
-    // Scroll to the current thumbnail
+    const handleScroll = () => {
+      if (stickyRef.current) {
+        const rect = stickyRef.current.getBoundingClientRect();
+        setIsSticky(rect.top <= 40);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
     if (scrollRef.current) {
       const thumbnail = scrollRef.current.children[currentIndex];
       if (thumbnail) {
@@ -38,7 +52,7 @@ const Carousel = ({ urls }) => {
 
   const scrollThumbnails = (direction) => {
     if (scrollRef.current) {
-      const scrollAmount = 100; // Adjust this value to control scroll distance
+      const scrollAmount = 100;
       scrollRef.current.scrollBy({
         left: direction === "next" ? scrollAmount : -scrollAmount,
         behavior: "smooth",
@@ -70,25 +84,29 @@ const Carousel = ({ urls }) => {
         </div>
       </div>
       <div
-        className="w-full flex justify-center top-8 bg-white sticky z-[999] block sm:hidden scroll-smooth"
-        // on click scroll to top
+        ref={stickyRef}
+        className={`w-full flex justify-center top-8 bg-white sticky z-[999] block sm:hidden scroll-smooth transition-all duration-300 ${
+          isSticky ? "py-1 shadow-md" : "py-2"
+        }`}
       >
         <button
           onClick={() => scrollThumbnails("prev")}
           className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-50 rounded-full p-1 hover:bg-opacity-75 transition-all"
         >
-          <ChevronLeft className="w-4 h-4 text-gray-800" />
+          <ChevronLeft
+            className={`${isSticky ? "w-3 h-3" : "w-4 h-4"} text-gray-800`}
+          />
         </button>
         <div
-          className="flex space-x-2 max-w-[22rem] overflow-x-auto scrollbar-hide py-2 px-1"
+          className="flex space-x-2 max-w-[22rem] overflow-x-auto scrollbar-hide px-1 py-2"
           ref={scrollRef}
         >
           {urls.map((url, index) => (
             <div
               key={index}
-              className={`w-20 h-20 flex-shrink-0 cursor-pointer rounded-lg overflow-hidden ${
-                currentIndex === index ? "ring-4 ring-blue-500" : ""
-              }`}
+              className={`flex-shrink-0 cursor-pointer rounded-lg overflow-hidden transition-all duration-300 ${
+                currentIndex === index ? "ring-2 ring-blue-500" : ""
+              } ${isSticky ? "w-14 h-14" : "w-20 h-20"}`}
               onClick={() => goToSlide(index)}
             >
               <img
@@ -103,7 +121,9 @@ const Carousel = ({ urls }) => {
           onClick={() => scrollThumbnails("next")}
           className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-50 rounded-full p-1 hover:bg-opacity-75 transition-all"
         >
-          <ChevronRight className="w-4 h-4 text-gray-800" />
+          <ChevronRight
+            className={`${isSticky ? "w-3 h-3" : "w-4 h-4"} text-gray-800`}
+          />
         </button>
       </div>
     </>
