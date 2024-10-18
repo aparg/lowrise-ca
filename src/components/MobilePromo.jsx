496e4845df249b9ece3d9e0169177d84f345ac48
aparg <apargtm@gmail.com>
2024-10-18 15:01:04 +0545

    style: mobile promo component

--- src/components/MobilePromo.jsx
+++ src/components/MobilePromo.jsx
@@ -4,7 +4,7 @@ import { MapPin } from "lucide-react";
 
 const MobilePromo = () => {
   return (
-    <div className="bg-gray-100 rounded-md p-8 md:p-12 lg:p-16 flex flex-col md:flex-row items-center justify-between relative my-60 hidden sm:block">
+    <div className="bg-gray-100 rounded-md p-8 md:py-12 lg:py-16 flex-col md:flex-row items-center justify-start relative my-60 hidden sm:block sm:px-56">
       <div className="mb-8 md:mb-0 md:mr-8 lg:mr-16">
         <h2 className="text-sm font-semibold text-gray-600 mb-2">
           MOBILE VERSION
@@ -15,12 +15,10 @@ const MobilePromo = () => {
           experience on your phone
         </h1>
         <p className="text-gray-600 mb-6">
-          Easy search for homes for sale
-          <br />
-          or lease across Ontario.
+          Easy search for homes for sale or lease across Ontario.
         </p>
       </div>
-      <div className="absolute right-0 top-0 w-full max-w-sm md:max-w-md lg:max-w-lg h-full flex items-center">
+      <div className="absolute right-8 top-0 w-full max-w-sm md:max-w-md lg:max-w-lg h-full flex items-center">
         <Image
           src="/images/lowrise-sc.png"
           alt="Mobile app screenshot"
