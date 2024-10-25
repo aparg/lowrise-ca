f5f8e5b173dd85c6445509f7c71d211bd750f16f
aparg <apargtm@gmail.com>
2024-10-25 23:26:08 +0545

    style: showcase images

--- src/components/CanadianCitiesShowcase.jsx
+++ src/components/CanadianCitiesShowcase.jsx
@@ -8,7 +8,7 @@ const CityCard = ({ name, imageUrl }) => (
       <img
         src={imageUrl}
         alt={`${name} cityscape`}
-        className="object-cover transition-transform duration-300 group-hover:scale-110 h-[250px] w-full"
+        className="object-cover transition-transform duration-300 group-hover:scale-110 h-[350px] w-full"
       />
       <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent" />
       <h3 className="absolute flex justify-center w-full bottom-4 text-white text-2xl font-bold text-center">
@@ -29,7 +29,7 @@ const CanadianCitiesShowcase = () => {
     { name: "Brampton", imageUrl: "/images/brampton.jpg" },
     { name: "Pickering", imageUrl: "/images/pickering.jpg" },
     { name: "Hamilton", imageUrl: "/images/hamilton.jpg" },
-    { name: "Oakville", imageUrl: "/images/oakville.jpg" },
+    // { name: "Oakville", imageUrl: "/images/oakville.jpg" },
     // Add more cities as needed
   ];
 
@@ -37,7 +37,7 @@ const CanadianCitiesShowcase = () => {
     <div className="my-[5rem] sm:my-[10rem]">
       <div className="mx-auto">
         <h2 className="text-4xl font-bold text-center mb-2 mt-10">
-          Low rise homes for sale in{" "}
+          Commercial Properties for sale in{" "}
           <span className="text-primary-green">your city </span>
           <span className="text-primary-green relative">
             <span className="absolute bottom-0 left-0 w-full h-1 bg-primary-gretext-primary-green rounded"></span>
@@ -46,7 +46,7 @@ const CanadianCitiesShowcase = () => {
         <p className="text-center text-gray-600 mb-12">
           Explore top cities across Canada
         </p>
-        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-x-2 gap-y-4 sm:gap-8">
+        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-x-2 gap-y-4 sm:gap-8">
           {cities.map((city) => (
             <CityCard
               key={city.name}
