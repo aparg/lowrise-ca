de713a5af432428118d75e49327b46acc1a667fc
aparg <apargtm@gmail.com>
2024-11-22 19:11:16 +0545

    fix: null case for average statistics

--- src/app/ontario/[city]/listings/[listingID]/page.js
+++ src/app/ontario/[city]/listings/[listingID]/page.js
@@ -73,8 +73,9 @@ const page = async ({ params }) => {
     municipality: main_data?.Municipality,
     saleLease: main_data?.SaleLease,
   });
-  main_data.avg =
-    parseFloat(statsValue?.avg?.toFixed(0)).toLocaleString() || null;
+  main_data.avg = statsValue?.avg
+    ? parseFloat(statsValue?.avg?.toFixed(0)).toLocaleString()
+    : null;
   const imageURLs = generateImageURLs(
     listingIDValue,
     parseInt(main_data?.PhotoCount)
